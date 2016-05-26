fbcde1881d548111aa1e26b8c52fcc6f07e6d062
shiwei <shiwei@fitcate.me>
2016-05-26 19:26:04 +0800

    first comit

---
+++ dubbo/dubbo-consumer/src/main/java/com/hotbody/dubbo/consumer/consumerAction.java
@@ -0,0 +1,38 @@
+package com.hotbody.dubbo.consumer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.hotbody.duboo.service.HelloService;
+
+public class consumerAction {
+	
+	private HelloService demoService;
+
+    
+
+	public HelloService getDemoService() {
+		return demoService;
+	}
+
+
+
+	public void setDemoService(HelloService demoService) {
+		this.demoService = demoService;
+	}
+
+
+
+	public void start() throws Exception {
+        for (int i = 0; i < Integer.MAX_VALUE; i ++) {
+            try {
+            	String hello = demoService.sayHello("world" + i);
+                System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] " + hello);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Thread.sleep(2000);
+        }
+	}
+
+}
